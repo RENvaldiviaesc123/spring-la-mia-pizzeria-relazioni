1250688fdc7537f8d12e4a67450025e9900881e0
renat <guitarra123primera@gmail.com>
2023-09-14 15:11:58 +0200

    Added Pizza Repository

--- src/main/java/org/learning/java/springlamiapizzeriacrud/model/Pizza.java
+++ src/main/java/org/learning/java/springlamiapizzeriacrud/model/Pizza.java
@@ -11,7 +11,7 @@ public class Pizza {
     //ATTRIBUTI
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
     private String name;
     private String description;
     private BigDecimal price;
@@ -22,7 +22,7 @@ public class Pizza {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
---
+++ src/main/java/org/learning/java/springlamiapizzeriacrud/repository/PizzaRepository.java
@@ -0,0 +1,8 @@
+package org.learning.java.springlamiapizzeriacrud.repository;
+
+
+import org.learning.java.springlamiapizzeriacrud.model.Pizza;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
+}
