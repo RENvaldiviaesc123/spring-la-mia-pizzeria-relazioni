c4066587dac2305049bca9913dde4b852b780ccc
renat <guitarra123primera@gmail.com>
2023-09-14 16:22:51 +0200

    HTML

---
+++ src/main/resources/templates/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO pizze (name, description, price) VALUES('Margueritta', 'Pomodoro e Mozzarella', '5');
+INSERT INTO pizze (name, description, price) VALUES('Prosciutto Cotto', 'Pomodoro, Mozzarella e Prosciutto Cotto', '6');
+INSERT INTO pizze (name, description, price) VALUES('Prosciutto Crudo', 'Pomodoro, Mozzarella e Prosciutto Crudo', '7.5');
