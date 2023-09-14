83cc902513e76e0e62f25c212902f4bc27fb191d
renat <guitarra123primera@gmail.com>
2023-09-14 15:21:36 +0200

    Pizza controller class done

--- src/main/java/org/learning/java/springlamiapizzeriacrud/controller/PizzaController.java
+++ src/main/java/org/learning/java/springlamiapizzeriacrud/controller/PizzaController.java
@@ -1,4 +1,28 @@
 package org.learning.java.springlamiapizzeriacrud.controller;
 
+
+import org.learning.java.springlamiapizzeriacrud.model.Pizza;
+import org.learning.java.springlamiapizzeriacrud.repository.PizzaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/pizze")
 public class PizzaController {
+
+    @Autowired
+    private PizzaRepository pizzaRepository;
+
+    @GetMapping
+    public String index(Model model) {
+        List<Pizza> pizzaList = pizzaRepository.findAll();
+        model.addAttribute("pizze", pizzaList);
+        return "/pizze/list";
+    }
+
 }
