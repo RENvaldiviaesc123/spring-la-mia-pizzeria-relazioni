7f09049549acb2010d06204744fba276ddb2ddfb
renat <guitarra123primera@gmail.com>
2023-09-18 14:55:36 +0200

    Added detail html, method to pizza details done too and added dettagli button to get to dettagli html.

--- src/main/java/org/learning/java/springlamiapizzeriacrud/controller/PizzaController.java
+++ src/main/java/org/learning/java/springlamiapizzeriacrud/controller/PizzaController.java
@@ -4,12 +4,16 @@ package org.learning.java.springlamiapizzeriacrud.controller;
 import org.learning.java.springlamiapizzeriacrud.model.Pizza;
 import org.learning.java.springlamiapizzeriacrud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/pizze")
@@ -22,8 +26,22 @@ public class PizzaController {
     public String index(Model model) {
         List<Pizza> pizzaList = pizzaRepository.findAll();
         model.addAttribute("pizze", pizzaList);
-        
+
         return "home";
+    }
+
+    //Aggiungiamo il metodo per vedere i dettagli della pizza
+    @GetMapping("/show/{pizzaid}")
+    public String show(@PathVariable("pizzaid") Integer id, Model model) {
+        Optional<Pizza> pizzaOptional = pizzaRepository.findById(id);
+
+        if (pizzaOptional.isPresent()) {
+            Pizza pizzaFromDb = pizzaOptional.get();
+            model.addAttribute("pizza", pizzaFromDb);
+            return "/detail";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 
 }
