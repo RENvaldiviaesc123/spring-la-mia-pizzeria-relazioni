3b574636a7a72bf483b12dec4f0b90550453046d
renat <guitarra123primera@gmail.com>
2023-09-26 17:39:07 +0200

    Fixed Offerte and OfferteController classes

--- src/main/java/org/learning/java/springlamiapizzeriacrud/model/Offerte.java
+++ src/main/java/org/learning/java/springlamiapizzeriacrud/model/Offerte.java
@@ -12,7 +12,7 @@ public class Offerte {
     //ATTRIBUTI
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private Integer id;
     @NotNull(message = "Inserisca una data di inizio")
     private LocalDate dataInizio;
     @NotNull(message = "Inserisca una data di fine ")
@@ -25,11 +25,11 @@ public class Offerte {
 
     //GETTER SETTER
 
-    public String getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
